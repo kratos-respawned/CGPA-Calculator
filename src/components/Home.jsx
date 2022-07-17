6f76ff6d907fe545bbef0e537efa1b7dbc36446a
Gaurav Bhandari <sbhandari1907@protonmail.com>
2022-07-17 22:31:45 +0530

    added reset button , improved logic

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { useState } from "react";
 
 let Home = () => {
@@ -6,24 +7,25 @@ let Home = () => {
   const [value, setValue] = useState(0);
   const [grade, setGrade] = useState(0);
   const [marks, setMarks] = useState(0);
+  const inputRef = useRef();
   let calculate = (number, credit) => {
     let num = 0;
 
     number >= 94
       ? (num = 10)
       : number >= 85 && number < 94
-        ? (num = 9)
-        : number >= 77 && number < 85
-          ? (num = 8)
-          : number >= 69 && number < 77
-            ? (num = 7)
-            : number >= 61 && number < 69
-              ? (num = 6)
-              : number >= 53 && number < 61
-                ? (num = 5)
-                : number >= 45 && number < 53
-                  ? (num = 4)
-                  : (num = 0);
+      ? (num = 9)
+      : number >= 77 && number < 85
+      ? (num = 8)
+      : number >= 69 && number < 77
+      ? (num = 7)
+      : number >= 61 && number < 69
+      ? (num = 6)
+      : number >= 53 && number < 61
+      ? (num = 5)
+      : number >= 45 && number < 53
+      ? (num = 4)
+      : (num = 0);
     return num * credit;
   };
   let changeHandler = (e) => {
@@ -40,6 +42,12 @@ let Home = () => {
     console.log(marks);
     iterator < 8 ? setIterator(iterator + 1) : setDisplay(2);
     setValue(0);
+    inputRef.current.focus();
+    e.preventDefault();
+  };
+  let Reset = (e) => {
+    setIterator(0);
+    setDisplay(1);
     e.preventDefault();
   };
   const subjects = [
@@ -80,63 +88,97 @@ let Home = () => {
       credit: 1,
     },
   ];
-  const glass= {
+  const glass = {
     backdropFilter: "blur(16px) saturate(180%)",
     webkitBackdropFilter: "blur(16px) saturate(180%)",
     backgroundColor: "rgba(0, 159, 121, 0.15)",
     borderRadius: "5px",
     borderBottom: "1px solid rgba(76, 215, 240, 0.041)",
-  }
+  };
   return (
     <div className="w-screen h-screen grid place-items-center bg-hero-pattern bg-cover font-OpenSans">
-      <div className="flex flex-col items-center justify-start p-5 w-[300px] h-auto" style={glass}>
-      <i className="fas fa-calculator text-[#2bae8f] text-[60px] my-2"></i>
-      <h1 className="text-white font-semibold my-3 text-2xl opacity-50">CGPA CALCULATOR</h1>
-      <form className="flex flex-col items-center justify-start w-full">
-        {display == 1 ? (
-          <>
-            <label className="text-white font-semibold my-3 text-2xl opacity-80 tracking-wider" htmlFor="marks">{subjects[iterator].subject}</label>
-            <input
-              className="w-full rounded-md shadow-md bg-[#cbfff38c] border-none outline-none py-2 px-2 placeholder:text-[#1117] placeholder:font-OpenSans placeholder:font-semibold text-[#1118] font-semibold"
-              required
-              placeholder="Enter marks"
-              type="number"
-              name="marks"
-              value={value}
-              onChange={changeHandler}
-            />
-            <button className="px-3 py-2 mt-5 bg-[#2bae8fc1] uppercase tracking-wider text-[#cbfff38c] font-medium hover:bg-[#2bae8f] hover:text-[#fff] hover:font-semibold rounded-sm transition shadow" onClick={clickHandler}>Next</button>
-            <br />
-          </>
-        ) : display == 2 ? (
-          <>
-            <label className="text-white font-semibold my-3 text-2xl opacity-80 tracking-wider" htmlFor="grade">IP Grade</label>
-            <select
-              className="w-full rounded-md shadow-md bg-[#cbfff38c] border-none outline-none py-2 px-2 placeholder:text-[#1117] placeholder:font-OpenSans placeholder:font-semibold text-[#1118] font-semibold"
-              name="grade"
-              value={grade}
-              onChange={(e) => {
-                setGrade(e.target.value);
-              }}
-            >
-              <option disabled value="0">
-                Enter Grades
-              </option>
-              <option value="10">A+</option>
-              <option value="9">A</option>
-              <option value="8">B+</option>
-              <option value="7">B</option>
-              <option value="6">C+</option>
-              <option value="5">C</option>
-            </select>
-            <button className="px-3 py-2 mt-5 bg-[#2bae8fc1] uppercase tracking-wider text-[#cbfff38c] font-medium hover:bg-[#2bae8f] hover:text-[#fff] hover:font-semibold rounded-sm transition shadow" onClick={gradeHandler}>Calculate</button>
-          </>
-        ) : (
-          <>
-            <h2 className="text-white font-semibold my-3 text-base opacity-90">CGPA :: {marks / 24.0}</h2>
-          </>
-        )}
-      </form>
+      <div
+        className="flex flex-col items-center justify-start p-5 w-[300px] h-auto"
+        style={glass}
+      >
+        <i className="fas fa-calculator text-[#2bae8f] text-[60px] my-2"></i>
+        <h1 className="text-white font-semibold my-3 text-2xl opacity-50">
+          CGPA CALCULATOR
+        </h1>
+        <form className="flex flex-col items-center justify-start w-full">
+          {display === 1 ? (
+            <>
+              <label
+                className="text-white font-semibold my-3 text-2xl opacity-80 tracking-wider"
+                htmlFor="marks"
+              >
+                {subjects[iterator].subject}
+              </label>
+              <input
+                className="w-full rounded-md shadow-md bg-[#cbfff38c] border-none outline-none py-2 px-2 placeholder:text-[#1117] placeholder:font-OpenSans placeholder:font-semibold text-[#1118] font-semibold"
+                required
+                placeholder="Enter marks"
+                type="number"
+                name="marks"
+                value={value}
+                onChange={changeHandler}
+                ref={inputRef}
+              />
+              <button
+                className="px-3 py-2 mt-5 bg-[#2bae8fc1] uppercase tracking-wider text-[#cbfff38c] font-medium hover:bg-[#2bae8f] hover:text-[#fff] hover:font-semibold rounded-sm transition shadow"
+                onClick={clickHandler}
+              >
+                Next
+              </button>
+              <br />
+            </>
+          ) : display === 2 ? (
+            <>
+              <label
+                className="text-white font-semibold my-3 text-2xl opacity-80 tracking-wider"
+                htmlFor="grade"
+              >
+                IP Grade
+              </label>
+              <select
+                className="w-full rounded-md shadow-md bg-[#cbfff38c] border-none outline-none py-2 px-2 placeholder:text-[#1117] placeholder:font-OpenSans placeholder:font-semibold text-[#1118] font-semibold"
+                name="grade"
+                value={grade}
+                onChange={(e) => {
+                  setGrade(e.target.value);
+                }}
+              >
+                <option disabled value="0">
+                  Enter Grades
+                </option>
+                <option value="10">A+</option>
+                <option value="9">A</option>
+                <option value="8">B+</option>
+                <option value="7">B</option>
+                <option value="6">C+</option>
+                <option value="5">C</option>
+              </select>
+              <button
+                className="px-3 py-2 mt-5 bg-[#2bae8fc1] uppercase tracking-wider text-[#cbfff38c] font-medium hover:bg-[#2bae8f] hover:text-[#fff] hover:font-semibold rounded-sm transition shadow"
+                onClick={gradeHandler}
+              >
+                Calculate
+              </button>
+            </>
+          ) : (
+            <>
+              <h2 className="text-white font-semibold my-3 text-base opacity-90">
+                CGPA :: {marks / 24.0}
+              </h2>
+              <button
+                className="px-3 py-2 mt-5 bg-[#2bae8fc1] uppercase tracking-wider text-[#cbfff38c] font-medium hover:bg-[#2bae8f] hover:text-[#fff] hover:font-semibold rounded-sm transition shadow"
+                onClick={Reset}
+              >
+                Reset
+              </button>
+            </>
+          )}
+        </form>
       </div>
     </div>
   );
